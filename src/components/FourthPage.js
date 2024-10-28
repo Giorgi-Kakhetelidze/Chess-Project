a81a29e7b5a4d3dd55fc198ccc6410ae5bd6bc0a
Giorgi Kakhetelidze <130541953+Giorgi-Kakhetelidze@users.noreply.github.com>
2024-10-28 18:09:30 +0000

    localStorage-changes

--- src/components/FourthPage.js
+++ src/components/FourthPage.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import crownLogo from "../imgs/crown.png";
 import LeftImg4 from "../imgs/fourthPage-left.png";
 import rocket from "../imgs/rocket.png";
 
 function FourthPage() {
+
+  const [storedName, setStoredName] = useState("");
+  const [storedMail, setStoredMail] = useState("");
+  const [storedPhone, setStoredPhone] = useState();
+
+
+  useEffect(() => {
+    const data = localStorage.getItem("userInfo"); 
+    if (data) {
+      const parsedData = JSON.parse(data); 
+      setStoredName(parsedData.name); 
+      setStoredMail(parsedData.email);
+      setStoredPhone(parsedData.phone)
+    }
+  }, []);
+
   return (
     <div className="firstPage">
       <div className="left-side">
@@ -11,14 +27,24 @@ function FourthPage() {
           <img src={crownLogo} alt="Crown Logo" />
           <h3>Redberry Knight Cup</h3>
         </div>
-        
-        <img src={LeftImg4} className="leftImg" alt="Left Side Image" />
+        <div className="leftImd-Div">
+          <img src={LeftImg4} className="leftImg" alt="Left Side Image" />
+          <div className="userInfo">
+            <h3>User Data:</h3>
+            <p>FullName: {storedName || "No data found"}</p>
+            <p>Email: {storedMail || "No data found"}</p>
+            <p>Phone Number: {storedPhone || "No data found"}</p>
+          </div>
+        </div>
       </div>
       <div className="rightSide">
         <div>
             <img src={rocket} alt="rocket" className="rocket" />
             <h2 className="onboarding">Onboarding completed!</h2>
         </div>
+
+        
+
       </div>
     </div>
   );
